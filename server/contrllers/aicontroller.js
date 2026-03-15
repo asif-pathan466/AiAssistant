e8442b7b644acbbac21222c951b92fe8313b70a7
asif pathan <asifpathan1266@gmail.com>
2026-03-15 02:48:44 -0400

    cange aicontroller file code

--- server/contrllers/aicontroller.js
+++ server/contrllers/aicontroller.js
@@ -1,25 +1,38 @@
 import sql from "../configs/db.js"
 import OpenAI from "openai";
 
+const openai = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
+
 export const message = async (req, res) => {
     try {
 
-        const { prompt } = req.body;
+        const { prompt, session_id } = req.body;
 
-        const openai = new OpenAI({
-            apiKey: process.env.GEMINI_API_KEY,
-            baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
-        });
+        // Save USER message
+        await sql`
+            INSERT INTO messages (session_id, sender_type, content)
+            VALUES (${session_id}, 'user', ${prompt})
+        `;
 
         const response = await openai.chat.completions.create({
             model: "gemini-3-flash-preview",
-            messages: [
-                { role: "user", content: prompt }
-            ],
+            messages: [{ role: "user", content: prompt }],
+            temperature: 0.7,
         });
 
+        const aiMessage = response.choices[0].message.content;
+
+        // Save AI message
+        await sql`
+            INSERT INTO messages (session_id, sender_type, content)
+            VALUES (${session_id}, 'ai', ${aiMessage})
+        `;
+
         res.json({
-            message: response.choices[0].message.content
+            message: aiMessage
         });
 
     } catch (error) {
